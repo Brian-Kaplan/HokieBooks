b27fa06ebf4108cd8900c2557bb60f204bec45b0
Brian Kaplan <bakaplan2@gmail.com>
2016-03-31 22:36:39 -0400

    first commit

---
+++ app/src/main/java/com/brian/hokiebooks/OauthConstants.java
@@ -0,0 +1,11 @@
+package com.brian.hokiebooks;
+
+/**
+ * Created by brian on 3/17/16.
+ */
+public class OauthConstants {
+    final static String REDIRECT_URL = "http://wso2.com";
+    final static String CLIENT_ID = "YwfFuNbEKSTqIAPlFUWGWmU1N1Ya";
+    final static String CLIENT_SECRET = "UmguKmp5qLOcEBloohvhUz5K3rUa";
+    final static String TRUSTSTORE_PASSWORD = "wso2carbon";
+}
