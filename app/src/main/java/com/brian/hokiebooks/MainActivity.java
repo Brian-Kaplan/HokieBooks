0f77b87149e0609f109a37df164992340cb20b84
Brian Kaplan <bakaplan2@gmail.com>
2016-04-13 15:45:25 -0400

    final revision

--- app/src/main/java/com/brian/hokiebooks/MainActivity.java
+++ app/src/main/java/com/brian/hokiebooks/MainActivity.java
@@ -9,26 +9,84 @@ import org.wso2.mobile.idp.proxy.IDPProxyActivity;
 import org.wso2.mobile.idp.proxy.IdentityProxy;
 import org.wso2.mobile.idp.proxy.beans.Token;
 import org.wso2.mobile.idp.proxy.callbacks.AccessTokenCallBack;
+import org.wso2.mobile.idp.proxy.handlers.AccessTokenHandler;
+import org.wso2.mobile.idp.proxy.handlers.RefreshTokenHandler;
 import org.wso2.mobile.idp.proxy.utils.ServerUtilities;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.media.session.MediaSession;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
 
 public class MainActivity extends IDPProxyActivity implements AccessTokenCallBack {
     private static String TAG = "MainActivity";
     Button button;
     Context context;
+    Token token;
     Menu menu;
+
+    private TextView accessTokenLabel = null;
+    private TextView tokenValidLabel = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        accessTokenLabel = (TextView) findViewById(R.id.accessTokenLabel);
+        tokenValidLabel = (TextView) findViewById(R.id.tokenValidLabel);
+
+        token = getSavedToken();
+        validateAccessToken();
+        accessTokenLabel.setText(getAccessToken());
+        if (getAccessToken() != null) {
+            Log.d("MainActivity", getAccessToken());
+        }
+
+        Button searchButton = (Button) findViewById(R.id.validateTokenButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tokenValidLabel.setText("Checking...");
+                validateAccessToken();
+            }
+        });
+
+        Button refreshTokenButton = (Button) findViewById(R.id.refreshTokenButton);
+        refreshTokenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RefreshTokenHandler e = new RefreshTokenHandler(OauthConstants.CLIENT_ID, OauthConstants.CLIENT_SECRET, token);
+                try {
+                    setSSL();
+                    IdentityProxy.getInstance().init(OauthConstants.CLIENT_ID, OauthConstants.CLIENT_SECRET, MainActivity.this);
+                    IdentityProxy.getInstance().setAccessTokenURL(getAccessTokenURL());
+                    e.obtainNewAccessToken();
+                } catch (InterruptedException | ExecutionException | TimeoutException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void validateAccessToken() {
+        APITask apiTask = (APITask) new APITask(new APITask.AsyncResponse() {
+            @Override
+            public void processFinish(Boolean output) {
+                if (output) {
+                    tokenValidLabel.setText("Valid Token");
+                } else
+                    tokenValidLabel.setText("Invalid Token");
+            }
+        }, getAccessToken()).execute();
     }
 
     @Override
@@ -36,6 +94,13 @@ public class MainActivity extends IDPProxyActivity implements AccessTokenCallBac
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         this.menu = menu;
+
+        String userName = getUserName();
+        if (userName != null) {
+            MenuItem item = menu.findItem(R.id.action_settings);
+            item.setTitle("Welcome : " + userName);
+        }
+
         return true;
     }
 
@@ -52,19 +117,40 @@ public class MainActivity extends IDPProxyActivity implements AccessTokenCallBac
 
         return super.onMenuItemSelected(featureId, item);
     }
+
+    public void onNewTokenReceived() {
+        try {
+            token = IdentityProxy.getInstance().getToken();
+            storeToken(token);
+            String accessToken = IdentityProxy.getInstance().getToken().getAccessToken();
+            String refreshToken = IdentityProxy.getInstance().getToken().getRefreshToken();
+            accessTokenLabel.setText(accessToken);
+            storeUserData(accessToken, refreshToken, getUserName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public void onTokenReceived() {
         try {
-            Token token = IdentityProxy.getInstance().getToken();
-            JSONObject mainObject = new JSONObject(IdentityProxy.getInstance().getToken().getIdToken().substring(27));
-            String subject = mainObject.getString("sub");
-            int index = subject.indexOf('@');
+            token = IdentityProxy.getInstance().getToken();
+            storeToken(token);
+            String jstring = IdentityProxy.getInstance().getToken().getIdToken();
+            String accessToken = IdentityProxy.getInstance().getToken().getAccessToken();
+            String refreshToken = IdentityProxy.getInstance().getToken().getRefreshToken();
+            String[] strings = jstring.split("\\}");
+            JSONObject payload = new JSONObject(strings[1]+"}");
+            String userName = payload.getString("sub");
+            int index = userName.indexOf('@');
             if(index>0){
-                subject = subject.substring(0, index);
+                userName = userName.substring(0, index);
             }
-            Log.v("Subject",subject);
+            Log.v("Subject",userName);
+            accessTokenLabel.setText(accessToken);
+            storeUserData(accessToken, refreshToken, userName);
             MenuItem item = menu.findItem(R.id.action_settings);
             //item.setVisible(false);
-            item.setTitle("Welcome : "+subject);
+            item.setTitle("Welcome : " + userName);
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -80,9 +166,59 @@ public class MainActivity extends IDPProxyActivity implements AccessTokenCallBac
         }
     }
 
+    private void storeToken(Token token) {
+        SharedPreferences sharedPreferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(token); // myObject - instance of MyObject
+        prefsEditor.putString("token", json);
+        prefsEditor.commit();
+    }
+
+    private Token getSavedToken() {
+        SharedPreferences sharedPreferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("token", null);
+        Token token = gson.fromJson(json, Token.class);
+        return token;
+    }
+
+
+    private void storeUserData(String accessToken, String refreshToken, String userName) {
+        SharedPreferences sharedPreferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("ACCESS_TOKEN", accessToken);
+        editor.putString("REFRESH_TOKEN", refreshToken);
+        editor.putString("USERNAME", userName);
+        editor.apply();
+    }
+
+    public String getAccessToken() {
+        SharedPreferences sharedPreferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
+        String accessToken = sharedPreferences.getString("ACCESS_TOKEN", null);
+        return accessToken;
+    }
+
+    public String getRefreshToken() {
+        SharedPreferences sharedPreferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
+        String refreshToken = sharedPreferences.getString("REFRESH_TOKEN", null);
+        return refreshToken;
+    }
+
+    private String getUserName() {
+        SharedPreferences sharedPreferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
+        String userName = sharedPreferences.getString("USERNAME", null);
+        return userName;
+    }
+
+    private String getAccessTokenURL() {
+        SharedPreferences sharedPreferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
+        String accessTokenUrl = sharedPreferences.getString("ACCESS_TOKEN_URL", null);
+        return accessTokenUrl;
+    }
+
     void setSSL(){
         InputStream inputStream = MainActivity.this.getResources().openRawResource(R.raw.truststore);
         ServerUtilities.enableSSL(inputStream, OauthConstants.TRUSTSTORE_PASSWORD);
     }
-
 }
--- app/src/main/java/com/brian/hokiebooks/OauthConstants.java
+++ app/src/main/java/com/brian/hokiebooks/OauthConstants.java
@@ -4,8 +4,8 @@ package com.brian.hokiebooks;
  * Created by brian on 3/17/16.
  */
 public class OauthConstants {
-    final static String REDIRECT_URL = "http://wso2.com";
-    final static String CLIENT_ID = "YwfFuNbEKSTqIAPlFUWGWmU1N1Ya";
-    final static String CLIENT_SECRET = "UmguKmp5qLOcEBloohvhUz5K3rUa";
+    final static String REDIRECT_URL = "http://google.com";
+    final static String CLIENT_ID = "egM3ZZIAcfUxZyEfLpGK4OE0Slwa";
+    final static String CLIENT_SECRET = "f35medprvnBqRcXPUZf3bCPwGzoa";
     final static String TRUSTSTORE_PASSWORD = "wso2carbon";
 }
